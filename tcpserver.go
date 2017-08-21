d44590793170dd3ac1dc1677928765689358aa5e
Antoino Grimaldo <grimi94@gmail.com>
2017-08-21 13:03:40 -0500

    Feat: improved error handling and added mongo integration

--- tcpserver.go
+++ tcpserver.go
@@ -5,7 +5,7 @@ import (
         "encoding/binary"
         "encoding/hex"
         "fmt"
-        // "gopkg.in/mgo.v2"
+        "gopkg.in/mgo.v2"
         "math"
         "net"
         "os"
@@ -20,7 +20,7 @@ const (
 )
 
 const PRECISION = 10000000.0
-const MONGO_DATABASE = "127.0.0.1:27010"
+const MONGO_DATABASE = ""
 const MONGO_PLACES_COLLECTION = ""
 const MONGO_RECORD_COLLECTION = ""
 const MONGO_URL = ""
@@ -32,16 +32,21 @@ type GPSElement struct {
         speed     int16
 }
 
+var pc *mgo.Collection = nil
+var rc *mgo.Collection = nil
+
 func main() {
         // Initialize mongo connector
-        // session, err := mgo.Dial(MONGO_URL)
-        // var c *mgo.Collection = nil
-        // if err != nil {
-        //         fmt.Println("mgo error: ", err.Error())
-        //         os.Exit(1)
-        // }
-        // c = session.DB(MONGO_DATABASE).C(MONGO_COLLECTION)
-        // c.Insert()
+        session, err := mgo.Dial(MONGO_URL)
+        if err != nil {
+                fmt.Println("mgo error: ", err.Error())
+                os.Exit(1)
+        }
+
+        pc = session.DB(MONGO_DATABASE).C(MONGO_PLACES_COLLECTION)
+        rc = session.DB(MONGO_DATABASE).C(MONGO_RECORD_COLLECTION)
+        fmt.Println(pc.Database)
+        fmt.Println(rc.Database)
 
         // Listen for incoming connections.
         l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
@@ -100,7 +105,20 @@ func handleRequest(conn net.Conn) {
                                 step++
                                 conn.Write(b)
                         case 2:
-                                elements := parseData(buf, size)
+                                elements, err := parseData(buf, size)
+                                if err != nil {
+                                        fmt.Println("Error while parsing data", err)
+                                        break
+                                }
+
+                                for i := 0; i < len(elements); i++ {
+                                        element := elements[i]
+                                        err := rc.Insert(&element)
+                                        if err != nil {
+                                                fmt.Println("Error inserting element to database", err)
+                                        }
+                                }
+
                                 conn.Write([]byte{0, 0, 0, uint8(len(elements))})
                         }
 
@@ -113,43 +131,53 @@ func handleRequest(conn net.Conn) {
         }
 }
 
-func parseData(data []byte, size int) []GPSElement {
+func parseData(data []byte, size int) (elements []GPSElement, err error) {
         reader := bytes.NewBuffer(data)
         fmt.Println("Reader Size:", reader.Len())
 
-        reader.Next(8)                                                      // header
-        reader.Next(1)                                                      // CodecID
-        recordNumber, _ := strconv.Atoi(hex.EncodeToString(reader.Next(1))) // Number of Records
+        // Header
+        reader.Next(4)                                                        // 4 Zero Bytes
+        dataLength, err := streamToInt32(reader.Next(4))                      // Header
+        reader.Next(1)                                                        // CodecID
+        recordNumber, err := strconv.Atoi(hex.EncodeToString(reader.Next(1))) // Number of Records
+        fmt.Println("Length of data:", dataLength)
 
-        elements := make([]GPSElement, recordNumber)
+        elements = make([]GPSElement, recordNumber)
 
         i := 0
         for i < recordNumber {
-                timestamp := streamToTime(reader.Next(8)) // Timestamp
-                reader.Next(1)                            // Priority
+                timestamp, err := streamToTime(reader.Next(8)) // Timestamp
+                reader.Next(1)                                 // Priority
 
                 // GPS Element
-                longitude := float64(streamToInt32(reader.Next(4))) / PRECISION // Longitude
-                latitude := float64(streamToInt32(reader.Next(4))) / PRECISION  // Latitude
+                longitudeInt, err := streamToInt32(reader.Next(4)) // Longitude
+                longitude := float64(longitudeInt) / PRECISION
+                latitudeInt, err := streamToInt32(reader.Next(4)) // Latitude
+                latitude := float64(latitudeInt) / PRECISION
 
-                reader.Next(2)                         // Altitude
-                angle := streamToInt16(reader.Next(2)) // Angle
-                reader.Next(1)                         // Satellites
-                speed := streamToInt16(reader.Next(2)) // Speed
+                reader.Next(2)                              // Altitude
+                angle, err := streamToInt16(reader.Next(2)) // Angle
+                reader.Next(1)                              // Satellites
+                speed, err := streamToInt16(reader.Next(2)) // Speed
+
+                if err != nil {
+                        fmt.Println("Error while reading GPS Element")
+                        break
+                }
 
                 elements[i] = GPSElement{longitude, latitude, angle, speed}
 
                 // IO Events Elements
 
                 reader.Next(1) // ioEventID
-                totalElements := streamToInt8(reader.Next(1))
-                fmt.Println("Total Elements:", totalElements)
+                reader.Next(1) // total Elements
 
                 stage := 1
                 for stage <= 4 {
-                        fmt.Println("Stage:", stage)
-                        stageElements := streamToInt8(reader.Next(1))
-                        fmt.Println("Stage Elements:", stageElements)
+                        stageElements, err := streamToInt8(reader.Next(1))
+                        if err != nil {
+                                break
+                        }
 
                         var j int8 = 0
                         for j < stageElements {
@@ -157,80 +185,69 @@ func parseData(data []byte, size int) []GPSElement {
 
                                 switch stage {
                                 case 1: // One byte IO Elements
-                                        _ = streamToInt8(reader.Next(1))
+                                        _, err = streamToInt8(reader.Next(1))
                                 case 2: // Two byte IO Elements
-                                        _ = streamToInt16(reader.Next(2))
+                                        _, err = streamToInt16(reader.Next(2))
                                 case 3: // Four byte IO Elements
-                                        _ = streamToInt32(reader.Next(4))
+                                        _, err = streamToInt32(reader.Next(4))
                                 case 4: // Eigth byte IO Elements
-                                        _ = streamToInt64(reader.Next(8))
+                                        _, err = streamToInt64(reader.Next(8))
                                 }
                                 j++
                         }
                         stage++
                 }
 
+                if err != nil {
+                        fmt.Println("Error while reading IO Elements")
+                        break
+                }
+
                 fmt.Println("Timestamp:", timestamp)
-                fmt.Println("Longitude:", longitude)
-                fmt.Println("Latitude:", latitude)
+                fmt.Println("Longitude:", longitude, "Latitude:", latitude)
 
                 i++
         }
 
         // Once finished with the records we read the Record Number and the CRC
 
-        _ = streamToInt8(reader.Next(1))  // Number of Records
-        _ = streamToInt32(reader.Next(4)) // CRC
+        _, err = streamToInt8(reader.Next(1))  // Number of Records
+        _, err = streamToInt32(reader.Next(4)) // CRC
 
-        return elements
+        return
 }
 
-func streamToInt8(data []byte) int8 {
+func streamToInt8(data []byte) (int8, error) {
         var y int8
         err := binary.Read(bytes.NewReader(data), binary.BigEndian, &y)
-        if err != nil {
-                panic(err)
-        }
-        return y
+        return y, err
 }
 
-func streamToInt16(data []byte) int16 {
+func streamToInt16(data []byte) (int16, error) {
         var y int16
         err := binary.Read(bytes.NewReader(data), binary.BigEndian, &y)
-        if err != nil {
-                panic(err)
-        }
-        return y
+        return y, err
 }
 
-func streamToInt32(data []byte) int32 {
+func streamToInt32(data []byte) (int32, error) {
         var y int32
         err := binary.Read(bytes.NewReader(data), binary.BigEndian, &y)
-        if err != nil {
-                panic(err)
-        }
         if y>>31 == 1 {
                 y *= -1
         }
-        return y
+        return y, err
 }
 
-func streamToInt64(data []byte) int64 {
+func streamToInt64(data []byte) (int64, error) {
         var y int64
         err := binary.Read(bytes.NewReader(data), binary.BigEndian, &y)
-        if err != nil {
-                panic(err)
-        }
-        return y
+        return y, err
 }
 
-func streamToFloat32(data []byte) float32 {
+func streamToFloat32(data []byte) (float32, error) {
         var y float32
         err := binary.Read(bytes.NewReader(data), binary.BigEndian, &y)
-        if err != nil {
-                panic(err)
-        }
-        return y
+        return y, err
 }
 
 func twos_complement(input int32) int32 {
@@ -238,10 +255,10 @@ func twos_complement(input int32) int32 {
         return -(input & mask) + (input &^ mask)
 }
 
-func streamToTime(data []byte) time.Time {
-        miliseconds := streamToInt64(data)
+func streamToTime(data []byte) (time.Time, error) {
+        miliseconds, err := streamToInt64(data)
         seconds := int64(float64(miliseconds) / 1000.0)
         nanoseconds := int64(miliseconds % 1000)
 
-        return time.Unix(seconds, nanoseconds)
+        return time.Unix(seconds, nanoseconds), err
 }
