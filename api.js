9b66424b5407aa2f839b30b59dbe62e2c5153c30
Antoino Grimaldo <grimi94@gmail.com>
2017-11-21 18:09:37 -0600

    Feat: now supports async/await

--- api.js
+++ api.js
@@ -7,7 +7,7 @@ const Place = require('./model/Place');
 const Record = require('./model/Record');
 
 const server = new Hapi.Server();
-const EARTH_RADIUS = 6378.1; // This is kilometers
+const EARTH_RADIUS = 6378.1; // This is in kilometers
 
 server.connection({ port: config.server.port, host: config.server.host });
 
@@ -23,50 +23,45 @@ server.route({
     config: {
         validate: {
             params:{
-                imei: joi.string()
+                imei: joi.string().length(34)
             },
             query: {
-                internalid: joi.number(), // Location internal id
-                radius: joi.number().min(0),
-                time: joi.date()
+                internalid: joi.number().integer(), // Location internal id
+                time: joi.date() // Time in UTC
             }
         }
     },
-    handler: function (request, reply) {
+    handler: async function (request, reply) {
         const start = new Date(request.query.time);
         const end   = addDays(start, 1);
 
-        Place.findOne({ internalid: request.query.internalid}).
-            exec(function(err, place){
-                if(err){
-                    reply("Unable to validate: Undefined location");
-                    return;
-                }
+        try{
+            const place = await Place.findOne({ internalid: request.query.internalid}).exec();
+            if(!place){
+                return reply("Unable to validate: Undefined location");
+            }
 
-                const area = {
-                    center: place.location.coordinates,
-                    radius: request.query.radius / EARTH_RADIUS,
-                    unique: true,
-                    spherical: true
-                };
+            const area = {
+                center: place.location.coordinates,
+                radius: (place.radius / 1000.0) / EARTH_RADIUS, // Convert radius to km
+                unique: true,
+                spherical: true
+            };
 
-                console.log("Validating:", request.params.imei, "With coordinates:", area.center, "And Radius:", area.radius);
-                console.log("Date From:", start, "To:", end);
+            console.log("Validating:", request.params.imei,
+                        "With coordinates:", area.center,
+                        "And Radius:", area.radius);
+            console.log("Date From:", start, "To:", end);
 
-                Record.find().
-                    where('time').gte(start).lt(end).
-                    where('location').within().circle(area).
-                    sort({ time: 'asc' }).
-                    limit(10).
-                    exec(function(err, record){
-                        if(err){
-                            reply(err);
-                        } else {
-                            reply(record);
-                        }
-                    });
+            const records = await Record.find().
+                where('time').gte(start).lt(end).
+                where('location').within().circle(area).
+                sort({ time: 'asc' }).exec();
 
-            });
+            return reply(records);
+        } catch (err) {
+            return reply(err);
+        }
     }
 });
 
@@ -76,25 +71,20 @@ server.route({
     config: {
         validate: {
             params:{
-                internalid: joi.number()
+                internalid: joi.number().integer()
             }
         }
     },
-    handler: function(request, reply) {
+    handler: async function(request, reply) {
         console.log("Retrieving information");
         var response;
 
-        Place.findOne({internalid: request.params.internalid})
-            .exec(function(err, place){
-                if(err){
-                    response = err;
-                } else {
-                    reponse = place;
-                }
-
-            });
-
-        reply(response);
+        try {
+            const place = await Place.findOne({internalid: request.params.internalid}).exec();
+            return reply(place);
+        } catch (err) {
+            return reply(err);
+        }
     }
 
 });
@@ -105,42 +95,36 @@ server.route({
     config: {
         validate: {
             params: {
-                internalid: joi.number()
+                internalid: joi.number().integer()
             },
             payload: {
-                latitude: joi.number(),
-                longitude: joi.number(),
-                radius: joi.number().integer().min(0)
+                latitude: joi.number().min(-90).max(90),
+                longitude: joi.number().min(-180).max(180),
+                radius: joi.number().integer().min(0) // Radius is in meters
             }
         }
     },
-    handler: function(request, reply) {
+    handler: async function(request, reply) {
         console.log('Inserting New Place:', request.params.internalid);
         console.log("Latitude", request.payload.latitude);
         console.log("Longitude", request.payload.longitude);
 
-        var response;
-        var place = new Place({
-            internalid: request.params.internalid,
-            location: {
-                type: "Point",
-                coordinates: [request.payload.longitude, request.payload.latitude]
-            },
-            radius: request.payload.radius
-        });
+        try {
+            const place = new Place({
+                internalid: request.params.internalid,
+                location: {
+                    type: "Point",
+                    coordinates: [request.payload.longitude, request.payload.latitude]
+                },
+                radius: request.payload.radius
+            });
 
-        place.save(function(err){
-            if(err){
-                console.log(err);
-                response = err;
-            } else {
-                response = "Succesful";
-            }
-        });
-
-        reply(response);
+            const savedPlace = await place.save();
+            return reply(savedPlace);
+        } catch(err) {
+            return reply(err);
+        }
     }
-
 });
 
 server.route({
@@ -152,24 +136,27 @@ server.route({
                 internalid: joi.number()
             },
             payload: {
-                latitude: joi.number(),
-                longitude: joi.number(),
-                radius: joi.number()
+                latitude: joi.number().min(-90).max(90),
+                longitude: joi.number().min(-180).max(180),
+                radius: joi.number().min(0)
             }
         }
     },
-    handler: function(request, reply) {
-        Place.update({
-            internalid: request.params.internalid
-        },{
-            location: [request.payload.longitude, request.payload.latitude]
-        }, {upsert: true}, function(err){
-            if(err){
-                reply(err);
-            } else {
-                reply("Succesful");
-            }
-        });
+    handler: async function(request, reply) {
+        try {
+            const updated = await Place.update({
+                internalid: request.params.internalid
+            }, {
+                radius: request.payload.radius,
+                location: {
+                    type: "Point",
+                    coordinates: [request.payload.longitude, request.payload.latitude]
+                }
+            }, {upsert: true}).exec();
+            return reply(updated);
+        } catch (err){
+            return reply(err);
+        }
     }
 });
 
@@ -183,8 +170,13 @@ server.route({
             }
         }
     },
-    handler: function(request, reply) {
-
+    handler: async function(request, reply) {
+        try {
+            const place = await Place.remove({internalid: request.params.internalid}).exec();
+            return reply(place);
+        } catch(err) {
+            return reply(err);
+        }
     }
 });
 
